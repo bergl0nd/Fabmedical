16bda819cf45e4d02c37e865c3bb68df2bcd02e1
Oscar Berglund <oscar.berglnd@gmail.com>
2021-10-26 12:27:49 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,11 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("578c246f-76c7-403b-b0f5-26edaa462da8");
+appInsights.start();
+
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://52.249.250.160:3001";
 
